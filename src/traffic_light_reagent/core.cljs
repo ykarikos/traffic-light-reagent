7eb778a18e7632d7db60d38379cf8796a5f940fe
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2019-02-02 14:18:02 +0200

    traffic light changing logic

--- src/traffic_light_reagent/core.cljs
+++ src/traffic_light_reagent/core.cljs
@@ -2,29 +2,53 @@
     (:require
       [reagent.core :as r]))
 
+(def opacity-cycle
+  ; Red  Yellow Green
+  [["1"   "0.2" "0.2"]
+   ["1"   "1"   "0.2"]
+   ["0.2" "0.2" "1"]
+   ["0.2" "1"   "0.2"]])
+
+(def third (fn [coll] (get coll 2)))
+
+(defn increment-cycle [n]
+  (mod (inc n) 4))
+
 ;; -------------------------
 ;; Views
 
-(defn home-page []
-  [:div {:style {:background "#000"
-                 :margin "auto"
-                 :width "120px"
-                 :padding "10px"
-                 :font-size "215px"
-                 :line-height "100px"
-                 :text-align "center"}}
-   [:div {:style {:color "#f00"
-                  :opacity "0.2"}} "●"]
-   [:div {:style {:color "#ff0"
-                  :opacity "0.2"}} "●"]
-   [:div {:style {:color "#0c0"
-                  :opacity "0.2"}} "●"]])
+(defn traffic-light []
+  (let [cycle-index (r/atom 0)]
+    (fn []
+      (js/setTimeout #(swap! cycle-index increment-cycle) 1500)
+      [:div {:style {:background "#000"
+                     :margin "auto"
+                     :width "120px"
+                     :padding "10px"
+                     :font-size "215px"
+                     :line-height "100px"
+                     :text-align "center"}}
+       [:div {:style {:color "#f00"
+                      :opacity (-> opacity-cycle
+                                   (get @cycle-index)
+                                   first)}}
+        "●"]
+       [:div {:style {:color "#ff0"
+                      :opacity (-> opacity-cycle
+                                   (get @cycle-index)
+                                   second)}}
+        "●"]
+       [:div {:style {:color "#0c0"
+                      :opacity (-> opacity-cycle
+                                   (get @cycle-index)
+                                   third)}}
+        "●"]])))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [traffic-light] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
