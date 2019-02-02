d6108707a9f9c45ffccd3472fad6c08379a74e0f
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2019-02-02 13:51:45 +0200

    remove external style

--- src/traffic_light_reagent/core.cljs
+++ src/traffic_light_reagent/core.cljs
@@ -6,10 +6,19 @@
 ;; Views
 
 (defn home-page []
-  [:div {:class "traffic-light"}
-   [:div {:class "light red"}]
-   [:div {:class "light yellow"}]
-   [:div {:class "light green"}]])
+  [:div {:style {:background "#000"
+                 :margin "auto"
+                 :width "120px"
+                 :padding "10px"
+                 :font-size "215px"
+                 :line-height "100px"
+                 :text-align "center"}}
+   [:div {:style {:color "#f00"
+                  :opacity "0.2"}} "●"]
+   [:div {:style {:color "#ff0"
+                  :opacity "0.2"}} "●"]
+   [:div {:style {:color "#0c0"
+                  :opacity "0.2"}} "●"]])
 
 ;; -------------------------
 ;; Initialize app
